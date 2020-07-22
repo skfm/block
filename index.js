2e2f3467e77c564f47a0abf35e43bfde08798952
SEIYA <skkasans@gmail.com>
2020-07-23 05:59:09 +0900

    パドルの作成

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
 
 canvas.width = 400;
 canvas.height = 400;
@@ -8,3 +9,38 @@ canvas.height = 400;
 canvas.setAttribute('style', 'display: block; margin: auto; background-color: #ddd');
 
 document.body.appendChild(canvas);
+
+const ball = {}
+
+const paddle = {
+  x: null,
+  y: null,
+  width: 100,
+  height: 15,
+  speed: 0,
+
+  update: function() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fill();
+  }
+}
+
+const block = {}
+
+const level = []
+
+const init = () => {
+  paddle.x = canvas.width / 2 - paddle.width / 2;
+  paddle.y = canvas.height - paddle.height;
+}
+const collide = () => {}
+
+const loop = () => {
+  paddle.update();
+
+  window.requestAnimationFrame(loop);
+}
+
+init();
+loop();
+

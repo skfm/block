e69f57d688cb8b1b5eade122ad5224489b91cec7
SEIYA <skkasans@gmail.com>
2020-07-23 05:51:30 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const canvas = document.createElement('canvas');
+
+canvas.width = 400;
+canvas.height = 400;
+
+canvas.setAttribute('style', 'display: block; margin: auto; background-color: #ddd');
+
+document.body.appendChild(canvas);
